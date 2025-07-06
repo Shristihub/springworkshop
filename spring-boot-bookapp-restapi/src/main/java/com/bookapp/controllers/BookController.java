1d044c53171c91e33104970d898d6d8f1fbe62db
shristitechacademy <sripriyamathan@gmail.com>
2025-07-06 16:50:04 +0530

    restapi code

---
+++ spring-boot-bookapp-restapi/src/main/java/com/bookapp/controllers/BookController.java
@@ -0,0 +1,17 @@
+package com.bookapp.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+//  REST api
+@RestController
+public class BookController {
+
+	
+	// methods -> restend point
+	// return type can be anything
+	
+	
+	
+	
+	
+}
